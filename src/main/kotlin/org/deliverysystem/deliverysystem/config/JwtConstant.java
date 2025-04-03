348eb10d5cc75e5cb9afdbbe9ad1a30dd8dc158d
Ankit Thosar <51828020+CracDoc@users.noreply.github.com>
2025-04-03 23:36:06 +0530

    Config Repository

---
+++ src/main/kotlin/org/deliverysystem/deliverysystem/config/JwtConstant.java
@@ -0,0 +1,10 @@
+package org.deliverysystem.deliverysystem.config;
+
+public class JwtConstant {
+
+
+    public static final String SECRET_KEY = "qwertyuioplkjhgfdsazxcvbnm";
+
+    public static final String JWT_HEADER = "Authorization";
+
+}
